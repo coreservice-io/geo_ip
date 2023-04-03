fcf4e8ad6b960bc5c865956b376c1363b4e23eb2
leo <leo@leos-MacBook-Pro.local>
2023-04-03 09:43:20 +0800

    update

--- data/util.go
+++ data/util.go
@@ -5,20 +5,40 @@ import (
 	"net"
 )
 
+var privateIPBlocks []*net.IPNet
+
+func init() {
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"169.254.0.0/16", // RFC3927 link-local
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+		"fc00::/7",       // IPv6 unique local addr
+	} {
+		_, block, _ := net.ParseCIDR(cidr)
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+}
+
 func IsLanIp(ip string) (bool, error) {
 	netIp := net.ParseIP(ip)
 	if netIp == nil {
-		return false, errors.New("ip error:" + ip)
+		return false, errors.New("ip format error:" + ip)
 	}
 
-	ip4 := netIp.To4()
-	if ip4 != nil {
-		if ip4[0] == 10 || ip[0] == 127 ||
-			(ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31) ||
-			(ip4[0] == 192 && ip4[1] == 168) {
+	if netIp.IsLoopback() || netIp.IsLinkLocalUnicast() || netIp.IsLinkLocalMulticast() {
+		return true, nil
+	}
+
+	for _, block := range privateIPBlocks {
+		if block.Contains(netIp) {
 			return true, nil
 		}
 	}
 
 	return false, nil
+
 }
